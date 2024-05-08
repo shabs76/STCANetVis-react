121a4ef75e8be1fc028b5e9537231872b8ec57d0
shabs76 <shabsmwanga76@gmail.com>
2024-05-08 14:32:59 +0300

    adding support for local windows deployment

--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.NODE_ENV === 'production' ? 'https://scanet.api.brentles.co.tz' : 'https://scanet.api.brentles.co.tz';
-// const URL = process.env.NODE_ENV === 'production' ? 'http://localhost:8000' : 'http://localhost:8000';
+// const URL = process.env.NODE_ENV === 'production' ? 'https://scanet.api.brentles.co.tz' : 'https://scanet.api.brentles.co.tz';
+const URL = process.env.NODE_ENV === 'production' ? 'http://localhost:390' : 'http://localhost:390';
 
 export const socket = io(URL);
